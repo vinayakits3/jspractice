d75020f4b5e9d6333c86169b6062e4bb4f535aef
vinayak <vinayak20130@gmail.com>
2024-04-07 17:02:19 +0530

    did more prac

---
+++ Comparisonofdatatypes.js
@@ -0,0 +1,11 @@
+console.log(`it is working`);
+console.log(2>1);
+console.log(2<1);
+console.log(2==2);
+
+console.log(`string and num`,'2'==2);
+console.log(`string and num ===`, '2' === 2);
+console.log(`ye btao`,"2">1);
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
---
+++ typeconversion.js
@@ -0,0 +1,29 @@
+// let num = 33;
+// console.log(typeof num);
+// let string = String(num);
+// console.log(typeof string);
+
+// ******************* Operations ******************
+
+let value = 3;
+let negvalue = -value;
+console.log(negvalue);
+
+console.log(2+2);
+// for power we use
+console.log(2**4);
+
+let str1 = "hello";
+let str2 = " Vinayak!";
+
+let str3 = str1+str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1+"2");
+console.log("1" + 2+2);
+console.log(2+2+"1");
+console.log('the value of +true is'+true);
+let gameCounter = 100;
+gameCounter++;
+console.log(`game counter is`,gameCounter);

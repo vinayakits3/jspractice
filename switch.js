f2c56e1dc6c85ceb6409ae5dd3b2834b5f1e2a4a
Vinayak verma <vinayak20130@gmail.com>
2024-06-20 18:40:23 +0530

    kk

---
+++ switch.js
@@ -0,0 +1,25 @@
+const month = 1;
+switch (month) {
+  case 1:
+    console.log(`January`);
+    break;
+  case 2:
+    console.log(`Feburary`);
+    break;
+  case 3:
+    console.log(`March`);
+    break;
+  case 4:
+    console.log(`April`);
+    break;
+  case 5:
+    console.log(`May`);
+    break;
+  case 6:
+    console.log(`June`);
+    break;
+
+  default:
+    console.log(`default`)
+    break;
+}
---
+++ truthy.js
@@ -0,0 +1,8 @@
+// const userEmail = "h@hitesh.ai"
+const userEmail = [];
+
+if (userEmail) {
+    console.log(`Got user email`);
+}else{
+    console.log(`Don't have user email`);
+}

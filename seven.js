0fee7825723e4197d229eac3ade6037cf2967ffe
vinayak <vinayak20130@gmail.com>
2024-07-07 19:03:20 +0530

    update

--- seven.js
+++ seven.js
@@ -1,3 +1,12 @@
-const myNumbers = [1,2,3,4,5,6,7,8,9,10];
-const addTen = myNumbers.map((value) => value +1)
-console.log(addTen)
+const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const addOne = myNumbers.map((value) => value +1)
+// console.log(addOne)
+
+const newNums = myNumbers
+  .map((value) => value * 10)
+  .map((val) => val + 1)
+  .filter((val) => val >= 40);
+
+  console.log(newNums);
+
+  // after this reduce
